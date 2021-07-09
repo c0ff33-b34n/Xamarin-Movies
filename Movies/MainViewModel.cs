4bd81fdbee7606053651bd10467eacc523bbb292
Stuart Bowes <stuartbowes2000@gmail.com>
2021-07-09 16:06:28 +0100

    Initialize project with initial Movie app, with a basic layout implemented and network service

---
+++ Movies/MainViewModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Text;
+using Xamarin.Forms;
+
+namespace Movies
+{
+    public class MainViewModel : BindableObject
+    {
+        public MainViewModel()
+        {
+            OnPropertyChanged("Items");
+        }
+
+        public ObservableCollection<string> Items => new ObservableCollection<string>
+        {
+            "Apocalypse Now",
+            "Full Metal Jacket",
+            "Battle of the River Plate"
+        };
+    }
+}
---
+++ Movies/NetworkService.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace Movies
+{
+    public class NetworkService
+    {
+        private HttpClient _httpClient;
+
+        public NetworkService()
+        {
+            _httpClient = new HttpClient();
+        }
+
+        public async Task<TResult> GetAsync<TResult>(string uri)
+        {
+            HttpResponseMessage response =  await _httpClient.GetAsync(uri);
+            string serialized = await response.Content.ReadAsStringAsync();
+            var result = JsonSerializer.Deserialize<TResult>(serialized);
+            return result;
+        }
+    }
+}
